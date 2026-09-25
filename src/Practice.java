50d4ce2d866a8e64eece4de2f28075f1a8dda495
Jessica H <jessicahebert75@gmail.com>
2026-09-24 22:58:05 -0700

    add recursive levelCount method for BinaryTreeNode

--- src/Practice.java
+++ src/Practice.java
@@ -152,7 +152,12 @@ public class Practice {
      * @return the number of levels in the tree
      */
     public static int levelCount(BinaryTreeNode<?> root) {
-        return 0;
+        if (root == null) return 0;
+
+        int leftCount = levelCount(root.left);
+        int rightCount= levelCount(root.right);
+
+        return Math.max(leftCount, rightCount)+1;
     }
 
 
